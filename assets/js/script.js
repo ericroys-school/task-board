80d158fb814f48810e95fd992ac3dcce51de42a0
eric.roys@gmail.com <eric.roys@gmail.com>
2024-05-11 16:56:47 -0500

    add modal form and basic validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -17,8 +18,25 @@ function renderTaskList() {
 
 }
 
+
+
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
+
+    /* add validation which displays error condition
+       below the applicable input on the form
+    */
+    const invalid = "is-invalid";
+    $( "#taskTitle, #taskDueDate, #taskDescription" ).each(function( index ) {
+        let x = null;
+        if(!(x = $(this).val()) || x === ""){
+            if(!$(this).hasClass(invalid))
+                $(this).addClass(invalid);
+        }else{
+            $(this).removeClass(invalid);
+        }
+      });
 
 }
 
@@ -32,7 +50,12 @@ function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Todo: when the page loads, render the task list, 
+//add event listeners, make lanes droppable, 
+//and make the due date field a date picker
 $(document).ready(function () {
-
+    // add date picker
+    $("#taskDueDate").datepicker();
+    //add listener to addTask button
+    $("#btnAddTask").on("click", handleAddTask);
 });
